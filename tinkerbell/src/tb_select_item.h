3e9515178d42833cbfd6b64342b6ae5c775c5a4a
Emil Segerås <emilsegers@gmail.com>
2013-01-20 22:20:37 +0100

    Changed from unsafe "user_ptr" to "TBValue tag" for custom item data.

--- tinkerbell/src/tb_select_item.h
+++ tinkerbell/src/tb_select_item.h
@@ -8,6 +8,7 @@
 
 #include "tb_linklist.h"
 #include "tb_list.h"
+#include "tb_value.h"
 
 namespace tinkerbell {
 
@@ -180,22 +181,21 @@ private:
 class TBGenericStringItem
 {
 public:
-	TBGenericStringItem(const TBGenericStringItem& other) : str(other.str), id(other.id), sub_source(other.sub_source), user_ptr(other.user_ptr) {}
-	TBGenericStringItem(const char *str) : str(str), sub_source(nullptr), user_ptr(nullptr) {}
-	TBGenericStringItem(const char *str, TBID id) : str(str), id(id), sub_source(nullptr), user_ptr(nullptr) {}
-	TBGenericStringItem(const char *str, TBSelectItemSource *sub_source) : str(str), sub_source(sub_source), user_ptr(nullptr) {}
-	const TBGenericStringItem& operator = (const TBGenericStringItem &other) { str.Set(other.str); id = other.id; sub_source = other.sub_source; user_ptr = other.user_ptr; return *this; }
+	TBGenericStringItem(const TBGenericStringItem& other) : str(other.str), id(other.id), sub_source(other.sub_source), tag(other.tag) {}
+	TBGenericStringItem(const char *str) : str(str), sub_source(nullptr) {}
+	TBGenericStringItem(const char *str, TBID id) : str(str), id(id), sub_source(nullptr) {}
+	TBGenericStringItem(const char *str, TBSelectItemSource *sub_source) : str(str), sub_source(sub_source) {}
+	const TBGenericStringItem& operator = (const TBGenericStringItem &other) { str.Set(other.str); id = other.id; sub_source = other.sub_source; tag = other.tag; return *this; }
 
-	void SetSkinImage(TBID image) { skin_image = image; }
-
-	void *GetUserPtr() { return user_ptr; }
-	void SetUserPtr(void *p) { user_ptr = p; }
+	void SetSkinImage(const TBID &image) { skin_image = image; }
 public:
 	TBStr str;
 	TBID id;
 	TBID skin_image;
 	TBSelectItemSource *sub_source;
-	void *user_ptr;
+
+	/** This value is free to use for anything. It's not used internally. */
+	TBValue tag;
 };
 
 /** TBGenericStringItemSource is a item source list providing items of type TBGenericStringItem. */
