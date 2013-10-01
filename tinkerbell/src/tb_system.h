c19f10c8116fb7481b7218f8b7541fcd79920ee9
Emil Segerås <emilsegers@gmail.com>
2013-10-01 20:53:41 +0200

    Added TBDebugPrint variadic macro.

--- tinkerbell/src/tb_system.h
+++ tinkerbell/src/tb_system.h
@@ -10,8 +10,10 @@
 
 #ifdef _DEBUG
 void TBDebugOut(const char *str);
+#define TBDebugPrint(str, ...) { TBStr tmp; tmp.SetFormatted(str, __VA_ARGS__); TBDebugOut(str); }
 #else
 #define TBDebugOut(str) ((void)0)
+#define TBDebugPrint(str, ...) ((void)0)
 #endif
 
 namespace tinkerbell {
