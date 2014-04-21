66e2c349bf662da6134b081b919bdb7a6559d945
Emil Segerås <emilsegers@gmail.com>
2014-04-21 14:51:53 +0200

    Switched to glfw version 3.0.4 for demo.

--- Demo/config.h
+++ Demo/config.h
@@ -4,6 +4,6 @@
 // This sucks, we need to define this to be able to build glfw without
 // an extra step after checkout (to generate config.h using cmake).
 
-#define _GLFW_VERSION_FULL "3.0.1"
+#define _GLFW_VERSION_FULL "3.0.4"
 
 #endif // DEMO_CONFIG_H
