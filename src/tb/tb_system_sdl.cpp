94fbf17727d35ef3d08034046f715ed0420f1286
michael tesch <tesch1@gmail.com>
2016-08-02 15:28:23 -0500

    reformat, fix compiler warning about uninitialized return

--- src/tb/tb_system_sdl.cpp
+++ src/tb/tb_system_sdl.cpp
@@ -11,6 +11,7 @@
 #include "tb_types.h"
 #include <sys/time.h>
 #include <stdio.h>
+#include <strings.h>
 
 // #ifdef __APPLE__
 // #include "SDL2/SDL.h"
@@ -126,41 +127,39 @@ int TBSystem::GetDPI()
 
 SDL_DisplayMode TBSystem::getDisplayMode()
 {
-  int i;
+	int ii = 0;
 
-  // Declare display mode structure to be filled in.
-  SDL_DisplayMode current;
+	// Declare display mode structure to be filled in.
+	SDL_DisplayMode current;
+	memset(&current, 0, sizeof(current));
 
-  // Get current display mode of all displays.
+	// Get current display mode of all displays.
+    int should_be_zero = SDL_GetCurrentDisplayMode(ii, &current);
 
-    int should_be_zero = SDL_GetCurrentDisplayMode(0, &current);
+    if (should_be_zero != 0)
+		// In case of error...
+		SDL_Log("Could not get display mode for video display #%d: %s", ii, SDL_GetError());
 
-    if(should_be_zero != 0)
-      // In case of error...
-      SDL_Log("Could not get display mode for video display #%d: %s", i, SDL_GetError());
-
-    else
-      // On success, print the current display mode.
-      return current;
-
+	// On success, print the current display mode.
+	return current;
 }
 
-int TBSystem::getScreenWidth (){
+int TBSystem::getScreenWidth()
+{
 	SDL_DisplayMode current = getDisplayMode();
-	 if(current.w)
-	 		return current.w;
-	 else
+	if (current.w)
+		return current.w;
+	else
 	 	return 600;
-
 }
 
-int TBSystem::getScreenHeight (){
+int TBSystem::getScreenHeight()
+{
 	SDL_DisplayMode current = getDisplayMode();
-	 if(current.h)
-	 		return current.h;
-	 else
-	 	return 400;
-
+	if (current.h)
+		return current.h;
+	else
+		return 400;
 }
 
 
