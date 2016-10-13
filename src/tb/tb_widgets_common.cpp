cc2a8fcbb4d74b74da74c4e355a7f81c8ecae643
michael tesch <tesch1@gmail.com>
2016-10-13 12:31:53 +0200

    keep movable objects within their parents limits

--- src/tb/tb_widgets_common.cpp
+++ src/tb/tb_widgets_common.cpp
@@ -748,8 +748,11 @@ bool TBMover::OnEvent(const TBWidgetEvent &ev)
 		if (target->GetParent())
 		{
 			// Apply limit.
-			rect.x = CLAMP(rect.x, -pointer_down_widget_x, target->GetParent()->GetRect().w - pointer_down_widget_x);
-			rect.y = CLAMP(rect.y, -pointer_down_widget_y, target->GetParent()->GetRect().h - pointer_down_widget_y);
+			//! fixed - the target xy should be constrained to its parent, not the pointer xy!
+			//rect.x = CLAMP(rect.x, -pointer_down_widget_x, target->GetParent()->GetRect().w - pointer_down_widget_x);
+			//rect.y = CLAMP(rect.y, -pointer_down_widget_y, target->GetParent()->GetRect().h - pointer_down_widget_y);
+			rect.x = CLAMP(rect.x, 0, target->GetParent()->GetRect().w - rect.w);
+			rect.y = CLAMP(rect.y, 0, target->GetParent()->GetRect().h - rect.h);
 		}
 		target->SetRect(rect);
 		return true;
