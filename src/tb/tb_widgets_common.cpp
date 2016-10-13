ae36827f322fb1a2ffc52d60b4aec57b7e35b4ec
michael tesch <tesch1@gmail.com>
2016-10-13 15:51:28 +0200

    let windows be moved off screen, just not completely

--- src/tb/tb_widgets_common.cpp
+++ src/tb/tb_widgets_common.cpp
@@ -751,8 +751,8 @@ bool TBMover::OnEvent(const TBWidgetEvent &ev)
 			//! fixed - the target xy should be constrained to its parent, not the pointer xy!
 			//rect.x = CLAMP(rect.x, -pointer_down_widget_x, target->GetParent()->GetRect().w - pointer_down_widget_x);
 			//rect.y = CLAMP(rect.y, -pointer_down_widget_y, target->GetParent()->GetRect().h - pointer_down_widget_y);
-			rect.x = CLAMP(rect.x, 0, target->GetParent()->GetRect().w - rect.w);
-			rect.y = CLAMP(rect.y, 0, target->GetParent()->GetRect().h - rect.h);
+			rect.x = CLAMP(rect.x, 0, target->GetParent()->GetRect().w - 50);
+			rect.y = CLAMP(rect.y, 0, target->GetParent()->GetRect().h - 50);
 		}
 		target->SetRect(rect);
 		return true;
