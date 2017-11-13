294a8bdae794a8d675f8aae2f7aae585e7b55747
Michael Tesch <tesch1@gmail.com>
2017-11-13 15:29:06 +0100

    shoulda been pure virtual

--- src/tb/tb_system.h
+++ src/tb/tb_system.h
@@ -82,7 +82,7 @@ public:
 	virtual long Size() = 0;
 	virtual size_t Read(void *buf, size_t elemSize, size_t count) = 0;
 	virtual size_t Write(const void *buf, size_t elemSize, size_t count) = 0;
-	virtual size_t Write(const TBStr & str);
+	virtual size_t Write(const TBStr & str) = 0;
 };
 
 } // namespace tb
