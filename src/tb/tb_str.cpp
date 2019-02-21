12997f966b8cde7fd0164ce38e59298edf649354
Michael Tesch <tesch1@gmail.com>
2019-02-21 15:09:03 +0100

    strndup for windows

--- src/tb/tb_str.cpp
+++ src/tb/tb_str.cpp
@@ -37,6 +37,26 @@ const char *stristr(const char *arg1, const char *arg2)
 	return nullptr;
 }
 
+#ifdef _WIN32
+#include <malloc.h>
+
+static char *strndup(const char *s, size_t n)
+{
+	char *result;
+	size_t len = strlen(s);
+
+	if (n < len)
+		len = n;
+
+	result = (char *)malloc(len + 1);
+	if (!result)
+		return 0;
+
+	result[len] = '\0';
+	return (char *)memcpy(result, s, len);
+}
+#endif // _WIN32
+
 // == TBStr ==========================================================
 
 TBStr::TBStr()
